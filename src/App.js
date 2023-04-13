4562dc2dd71f7515cf773d630c33e52a4c924803
RamandHawf <h.umerusman786@gmail.com>
2023-04-13 16:24:06 +0500

    Code Commit Crypto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Aboutus from './Components/Aboutus';
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Aboutus/>
+    </>
+  );
+}
+
+export default App;
