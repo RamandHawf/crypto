607b7337a514584e2955003307270976affe087c
RamandHawf <h.umerusman786@gmail.com>
2023-04-16 04:09:14 +0500

    Code Seperate css

--- src/Components/Aboutus.js
+++ src/Components/Aboutus.js
@@ -1,304 +1,63 @@
-import React from 'react';
-
+import React from "react";
+import "./about.css";
 const Aboutus = () => {
-    return (
-        <div className='aboutparent' style={{
-            marginTop: "177px",
-            height: "1754px",
-            width: "100%",
-            backgroundColor: "#1E1E1E"
-        }}>
-            <div className='logoup' style={{
-                height: "12.73px",
-                width: "100%",
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: 'center',
-                alignContent: 'center'
-
-            }}>
-                <div className='pic' style={{ marginTop: '3px', width: "76.75px", height: '12.72px' }} >
-                    <img src="top.png" alt="top" />
-                </div>
-            </div>
-
-            <div className='middlebodydata' style={{
-                marginTop: "120.21px",
-                height: "361px",
-                width: "100%",
-                display: 'flex',
-                flexDirection: "row",
-            }} >
-                <div className='videodata' style={{
-                    height: '361px',
-                    width: '519px',
-                    border: "2px solid yellow",
-                    backgroundColor: "white",
-                    marginLeft: '172px'
-
-                }}>
-
-                </div>
-                <div className='textdata' style={{
-                    margin: '55px',
-                    marginLeft: '120px',
-                    height: '252px',
-                    width: '458px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                }}>
-                    <div className='a' style={{
-                        width: "117px",
-                        height: "20px",
-
-
-                        fontFamily: 'MuseoModerno',
-                        fontStyle: "normal",
-                        fontWeight: "700",
-                        fontSize: "18px",
-                        lineHeight: "20px",
-                        /* identical to box height, or 111% */
-
-                        letterSpacing: "0.2em",
-                        textTransform: "uppercase",
-
-                        background: "linear-gradient(68.58deg, #883BDE 9%, #88D5DE 97.66%), #C004DE",
-                        webkitBackgroundClip: "text",
-                        webkitTextFillColor: "transparent",
-                        backgroundClip: "text",
-                        textFillColor: "transparent",
-                    }} >About Us</div>
-                    <div className='b' style={{
-                        marginTop: 'px',
-                        width: "443px",
-                        height: "56px",
-
-
-                        fontFamily: 'MuseoModerno',
-                        fontStyle: "normal",
-                        fontWeight: "700",
-                        fontSize: "48px",
-                        lineHeight: "56px",
-                        color: "#FFFFFF",
-                    }} > What is optimos ? </div>
-
-                    <div className='c' style={{
-
-                        marginTop: 'px',
-                        width: "443px",
-                        height: "152px",
-
-
-                        fontFamily: 'MuseoModerno',
-                        fontStyle: "normal",
-                        fontWeight: "700",
-                        fontSize: "24px",
-                        lineHeight: "38px",
-                        color: "rgba(255, 255, 255, 0.9)"
-                    }} >
-                        Optimos offer music artists the ability
-                        to connect with each other along with
-                        investors and digitally exchange NFTs
-                        for their with in.....
-
-                    </div>
-
-                </div>
-            </div>
-
-            <div className='logobottom' style={{
-                height: "12.73px",
-                width: "100%",
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: 'center',
-                alignContent: 'center'
-
-            }}>
-                <div className='pic' style={{
-
-                    marginTop: '220px', width: "76.75px", height: '12.72px'
-                }} >
-                    <img src="top.png" alt="top" />
-                </div>
-            </div>
-
-            <div className='documentData' style={{
-                marginTop:"240.21px",
-                display:'flex',
-               width:"100%",
-
-                flexDirection:'row',
-                justifyContent:'center',
-                alignContent:'center',
-                alignItems:'center'
-
-            }}>
-            <div className='dd1' style={{
-            //    marginLeft:"20px",  
-                 width:"1242px",
-                             height:"372px",
-                            //  backgroundColor:'yellow',
-            }}>
-                <div className='dd1child-pf1' 
-                style={{
-                    display:'flex',
-                    flexDirection:'row',
-                    justifyContent:'center',
-                    alignContent:'center',
-                    alignItems:'center',
-
-                }}
-                >
-                 <h1 className='hedaingofdocument' style={{
-                    margin:0,
-                    padding:0,
-                    width: '238px',
-                    height: '56px',
-                    fontFamily: 'MuseoModerno',
-                    fontStyle: 'normal',
-                    fontWeight: 700,
-                    fontSize: '48px',
-                    lineHeight: '56px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    textTransform: 'capitalize',
-                    color: '#FFFFFF'
-                 }} > Document </h1>
-
-                </div>
-
-                <div className='dd1child-pf2' 
-                style={{
-                    marginTop:"0px",
-                    display:'flex',
-                    flexDirection:'row',
-                    justifyContent:'center',
-                    alignContent:'center',
-                    alignItems:'center',
-
-                }}
-                >
-                 <h1 className='hedaingofdocumentdetail' style={{
-                       margin:0,
-                       padding:0, 
-                       width: '1065px',
-                       height: '76px',
-                       fontFamily: 'MuseoModerno',
-                       fontStyle: 'normal',
-                       fontWeight: 400,
-                       fontSize: '24px',
-                       lineHeight: '38px',
-                       display: 'flex',
-                       alignItems: 'center',
-                       textAlign: 'center',
-                       textTransform: 'capitalize',
-                       color: '#FFFFFF'
-                 }} > Document the whote paper and learn about the optimos Token, The Unique ICO Crypto Approach and the Team/Advisors. </h1>
-
-                </div>
-
-                  <div className='boxes' style={{
-                    marginTop:"48px",
-                    height:'176px',
-                    width:'100%',
-                    display:"flex",
-                    flexDirection:'row',
-                    // backgroundColor:'wheat'
-
-                  }}>
-                  <div className='box1' style={{
-                    width:'292.5px',
-                    // backgroundColor:"navajowhite",
-                    // marginLeft:'100px',
-                    boxSizing: "border-box",
-                    // position: "absolute",
-                    // left: "6.88%",
-                    right: "72.81%",
-                    top: "40.37%",
-                    bottom: "55.88%",
-                    border: "1px solid #2E2E2E",
-                    borderRadius: "10px"
-                  
-                  }}
-                  >
-                  <h1 style={{
-                    // position: "absolute",
-                    width: "136px",
-                    height: "32px",
-                    left: "calc(50% - 136px/2 - 475px)",
-                    top: "calc(50% - 32px/2 - 364px)",
-                    fontFamily: "'MuseoModerno'",
-                    fontStyle: "normal",
-                    fontWeight: "600",
-                    fontSize: "24px",
-                    lineHeight: "32px",
-                    textAlign: "center",
-                    textTransform: "capitalize",
-                    color: "#FFFFFF",
-                    opacity: "0.8"
-                  }} >Paper</h1>
-                  </div>
-                  <div className='box2'  style={{
-                    marginLeft:"24px",
-                    width:'292.5px',
-                    // backgroundColor:"",
-                    // marginLeft:'100px',
-                    boxSizing: "border-box",
-                    // position: "absolute",
-                    // left: "6.88%",
-                    right: "72.81%",
-                    top: "40.37%",
-                    bottom: "55.88%",
-                    border: "1px solid #2E2E2E",
-                    borderRadius: "10px"
-                  
-                  }} >
-                    WHITE PAPER   
-                  </div>
-                  <div className='box3'  style={{
-                    marginLeft:"24px",
-                    width:'292.5px',
-                    // backgroundColor:"",
-                    // marginLeft:'100px',
-                    boxSizing: "border-box",
-                    // position: "absolute",
-                    // left: "6.88%",
-                    right: "72.81%",
-                    top: "40.37%",
-                    bottom: "55.88%",
-                    border: "1px solid #2E2E2E",
-                    borderRadius: "10px"
-                  
-                  }} >
-                    PRIVACY POLICY   
-                  </div>
-                  <div className='box4'  style={{
-                    marginLeft:"24px",
-                    width:'292.5px',
-                    // backgroundColor:"",
-                    // marginLeft:'100px',
-                    boxSizing: "border-box",
-                    // position: "absolute",
-                    // left: "6.88%",
-                    right: "72.81%",
-                    top: "40.37%",
-                    bottom: "55.88%",
-                    border: "1px solid #2E2E2E",
-                    borderRadius: "10px"
-                  
-                  }} >
-                    TERM OF COIN SALES   
-                  </div>
-                  </div>
-            </div>
-            
-            
-            </div>
+  return (
+    <div className="aboutparent">
+      <div className="logoup">
+        <div className="pic">
+          <img src="top.png" alt="top" />
         </div>
-    );
-}
+      </div>
+
+      <div className="middlebodydata">
+        <div className="videodata"></div>
+        <div className="textdata">
+          <div className="a">About Us</div>
+          <div className="b">What is optimos?</div>
+          <div className="c">
+            Optimos offer music artists the ability to connect with each other
+            along with investors and digitally exchange NFTs for their with
+            in.....
+          </div>
+        </div>
+      </div>
+
+      <div class="logobottom">
+        <div class="pic">
+          <img src="top.png" alt="top" />
+        </div>
+      </div>
+      <div className="documentData">
+        <div className="dd1">
+          <div className="dd1child-pf1">
+            <h1 className="hedaingofdocument"> Document </h1>
+          </div>
+
+          <div className="dd1child-pf2">
+            <h1 className="hedaingofdocumentdetail">
+              {" "}
+              Document the whote paper and learn about the optimos Token, The
+              Unique ICO Crypto Approach and the Team/Advisors.{" "}
+            </h1>
+          </div>
+          <div className="boxes">
+            <div className="box1">
+              <h1 className="ONEPAPER">ONE PAPER</h1>
+            </div>
+            <div className="box2">
+              <h1 className="whitepaper">WHITE PAPER</h1>
+            </div>
+            <div className="box3">
+              <h1 className="privacypolicy">PRIVACY POLICY</h1>
+            </div>
+            <div className="box4">
+              <h1 className="coins">TERM OF COIN SALES</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Aboutus;
